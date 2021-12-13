aab6178f8763e6bb3bf0ecad3b6a6c7ce186cea4
Michael Morf <michael.morf@kcc.at>
2021-12-13 21:40:40 +0100

    level1

---
+++ level1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+int count = 0;
+int? prev = null;
+List<int> values = File.ReadAllLines("input.txt").Select(t => int.Parse(t)).ToList();
+List<string> output = new List<string>();
+
+foreach (var value in values) {
+    if (prev == null)
+    {
+        prev = value;
+    }
+    else
+    {
+        if (value > prev) count++;
+    }
+
+    prev = value;
+}
+
+Console.WriteLine(count);
