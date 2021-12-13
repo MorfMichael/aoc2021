f38496a435b94c15393df36c062f3cdac46667fc
Michael Morf <michael.morf@kcc.at>
2021-12-13 23:57:18 +0100

    level3 part1

--- level3/Program.cs
+++ level3/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+using System.Linq;
+
+string a = "";
+string b = "";
+var input = File.ReadAllLines("input.txt").SelectMany(x => x.Select((t,i) => (Index: i, Char: t))).GroupBy(x => x.Index, x => x.Char).ToList();
+
+foreach (var index in input) 
+{
+    if (index.Count(x => x == '1') > index.Count(x => x == '0'))
+    {
+        a += '1';
+        b += '0';
+    }
+    else 
+    {
+        a += '0';
+        b += '1';
+    }
+}
+
+int result = Convert.ToInt32(a,2) * Convert.ToInt32(b,2);
+File.WriteAllText("output.txt", result.ToString());
+Console.WriteLine(result);
