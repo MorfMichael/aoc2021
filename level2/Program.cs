ac8961d88931feb2af8ebfb4372b62464dabf52d
Michael Morf <michael.morf@kcc.at>
2021-12-13 22:14:37 +0100

    level2 finish

--- level2/Program.cs
+++ level2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+using System.Linq;
+
+int? prev = null;
+int count = 0;
+List<int> input = File.ReadAllLines("input.txt").Select(t => int.Parse(t)).ToList();
+
+for (int i = 0; i < input.Count; i++) 
+{
+    int sum = input.Skip(i).Take(3).Sum();
+    if (prev.HasValue && sum > prev) count++;
+    prev = sum;
+}
+
+File.WriteAllText("output.txt", count.ToString());
+Console.WriteLine(count);
