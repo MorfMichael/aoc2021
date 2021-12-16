c9f6eb87049e553ce9352447abdff65a66a17f08
Michael Morf <michael.morf@kcc.at>
2021-12-17 00:05:12 +0100

    level6

---
+++ level6/Program.cs
@@ -0,0 +1,31 @@
+﻿string input = File.ReadAllText("input.txt");
+List<int> fishis = input.Split(",").Select(t => int.Parse(t)).ToList();
+int days = 256;
+
+List<int> toAdd = new List<int>();
+
+Console.WriteLine($"initial state: {string.Join(",", fishis)}");
+
+for (int day = 1; day <= days; day++)
+{
+    for (int i = 0; i < fishis.Count; i++)
+    {
+        if (fishis[i] > 0)
+        {
+            fishis[i]--;
+        }
+        else
+        {
+            fishis[i] = 6;
+            toAdd.Add(8);
+        }
+    }
+
+    fishis.AddRange(toAdd);
+    toAdd.Clear();
+
+    //Console.WriteLine($"After {day.ToString().PadLeft(2)} days: {string.Join(",", fishis)}");
+
+}
+
+Console.WriteLine(fishis.Count);
