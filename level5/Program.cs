eaf762ede27a323a607bdc8bf9fc4ff54efa619e
Michael Morf <michael.morf@kcc.at>
2021-12-16 21:59:25 +0100

    level5 win

--- level5/Program.cs
+++ level5/Program.cs
@@ -15,15 +15,34 @@ List<(int X, int Y)> result = new List<(int X, int Y)>();
 
 foreach (var entry in data)
 {
-    if (entry.Start.X != entry.End.X || entry.Start.Y != entry.End.Y)
+    if (!(entry.Start.X == entry.End.X || entry.Start.Y == entry.End.Y))
         continue;
 
     if (entry.Start.X == entry.End.X)
     {
-        
+        int min = Min(entry.Start.Y, entry.End.Y);
+        int max = Max(entry.Start.Y, entry.End.Y);
+        for (int i = min; i <= max; i++)
+        {
+            result.Add((entry.Start.X, i));
+        }
     }
-    else if (entry.Start.Y == entry.End.Y)
+   
+    if (entry.Start.Y == entry.End.Y)
     {
-
+        int min = Min(entry.Start.X, entry.End.X);
+        int max = Max(entry.Start.X, entry.End.X);
+        for (int i = min; i <= max; i++)
+        {
+            result.Add((i, entry.Start.Y));
+        }
     }
 }
+
+int output = result.GroupBy(x => x).Where(t => t.Count() > 1).Count();
+File.WriteAllText("output.txt", output.ToString());
+Console.WriteLine(output);
+
+
+int Min(int a, int b) => a < b ? a : b;
+int Max(int a, int b) => a > b ? a : b;
