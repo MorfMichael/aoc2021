382db77cb0ccbca2cfd2ded221c5d09d58fbad81
Michael Morf <michael.morf@kcc.at>
2021-12-19 12:57:47 +0100

    level 8 part 1 win

---
+++ level8/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] input = File.ReadAllLines("input.txt");
+
+List<(string[] Input, string[] Output)> signals = input.Select(t =>
+{
+    var split = t.Split("|");
+    return (Input: split[0].Split(" ", StringSplitOptions.RemoveEmptyEntries), Output: split[1].Split(" ", StringSplitOptions.RemoveEmptyEntries));
+}).ToList();
+
+Console.WriteLine(signals.Sum(x => x.Output.Count(t => new int[] { 2, 3, 4, 7 }.Contains(t.Length))));
