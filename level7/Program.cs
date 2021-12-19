21427cb79592ddaed353357b89f4497e85a43a71
Michael Morf <michael.morf@kcc.at>
2021-12-19 11:52:32 +0100

    level7 part 1 win

---
+++ level7/Program.cs
@@ -0,0 +1,15 @@
+﻿string input = File.ReadAllText("input.txt");
+List<int> positions = input.Split(",").Select(t => int.Parse(t)).ToList();
+
+int max = positions.Max();
+int minSum = 100000000;
+for (int i = 0; i < max; i++)
+{
+    int sum = positions.Sum(x => Calculate(x, i));
+    if (sum < minSum)
+        minSum = sum;
+}
+
+Console.WriteLine(minSum);
+
+int Calculate(int current, int target) => Math.Abs(current - target);
