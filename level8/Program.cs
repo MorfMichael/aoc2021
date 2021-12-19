d38f6f7316ab62150f679e3f12f44ef7176bc799
Michael Morf <michael.morf@kcc.at>
2021-12-19 14:42:32 +0100

    level 8 start

--- level8/Program.cs
+++ level8/Program.cs
@@ -2,8 +2,10 @@
 
 List<(string[] Input, string[] Output)> signals = input.Select(t =>
 {
-    var split = t.Split("|");
-    return (Input: split[0].Split(" ", StringSplitOptions.RemoveEmptyEntries), Output: split[1].Split(" ", StringSplitOptions.RemoveEmptyEntries));
+    var split = t.Split(" | ");
+    return (Input: split[0].Split(), Output: split[1].Split());
 }).ToList();
 
-Console.WriteLine(signals.Sum(x => x.Output.Count(t => new int[] { 2, 3, 4, 7 }.Contains(t.Length))));
+var first = input[0];
+
+Console.WriteLine(first);
