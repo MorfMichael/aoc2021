7af2be130a4245015a2794402343a557816513be
Michael Morf <michael.morf@kcc.at>
2021-12-24 19:10:04 +0100

    level10 part 1

---
+++ level10/Program.cs
@@ -0,0 +1,44 @@
+﻿//string[] lines = File.ReadAllLines("example.txt");
+string[] lines = File.ReadAllLines("input.txt");
+
+List<(char expected, char got)> illegal = new List<(char,char)>();
+
+string close = ">)]}";
+string open = "<([{";
+
+Dictionary<char, int> values = new Dictionary<char, int>
+{
+    { ')', 3 },
+    { ']', 57 },
+    { '}', 1197 },
+    { '>', 25137 },
+};
+
+foreach (var line in lines)
+{
+    string x = "";
+
+    foreach (var c in line)
+    {
+        if (close.Contains(c))
+        {
+            char expected = close[open.IndexOf(x.Last())];
+            if (c == expected)
+            {
+                x = x[..^1];
+            }
+            else
+            {
+                illegal.Add((expected,c));
+                break;
+            }
+        }
+        else
+        {
+            x += c;
+        }
+    }
+}
+
+int score = illegal.Sum(x => values[x.got]);
+Console.WriteLine(score);
