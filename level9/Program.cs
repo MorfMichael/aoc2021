112e55e129dcb03c5104500cc203219d6667dbb9
Michael Morf <michael.morf@kcc.at>
2021-12-24 18:09:09 +0100

    level9 part 1 with class

--- level9/Program.cs
+++ level9/Program.cs
@@ -1,73 +1,47 @@
 ﻿string[] lines = File.ReadAllLines("input.txt");
 //string[] lines = File.ReadAllLines("example.txt");
 
-int[][] map = lines.Select(t => t.Select(x => int.Parse(x.ToString())).ToArray()).ToArray();
+List<Point> map = lines.SelectMany((line,y) => line.Select((cell,x) => new Point(x,y,int.Parse(cell.ToString()), line.Length, lines.Length))).ToList();
 
 List<int> lowest = new List<int>();
 
-
-for (int i = 0; i < map.Length; i++)
+foreach (var point in map)
 {
-    for (int j = 0; j < map[i].Length; j++)
-    {
-        int value = map[i][j];
+    var neighbours = point.GetNeighbours().Select(t => map.FirstOrDefault(x => x.X == t.X && x.Y == t.Y)).ToList();
 
-        if (i == 0)
-        {
-            if (j == 0)
-            {
-                if (map[i + 1][j] > value && map[i][j+1]> value)
-                    lowest.Add(value);
-            }
-            else if (j == map[i].Length - 1)
-            {
-                if (map[i + 1][j] > value && map[i][j - 1] > value)
-                    lowest.Add(value);
-            }
-            else
-            {
-                if (map[i + 1][j] > value && map[i][j - 1] > value && map[i][j + 1] > value)
-                    lowest.Add(value);
-            }
-        }
-        else if (i == map.Length - 1)
-        {
-            if (j == 0)
-            {
-                if (map[i - 1][j] > value && map[i][j + 1] > value)
-                    lowest.Add(value);
-            }
-            else if (j == map[i].Length - 1)
-            {
-                if (map[i - 1][j] > value && map[i][j - 1] > value)
-                    lowest.Add(value);
-            }
-            else
-            {
-                if (map[i - 1][j] > value && map[i][j - 1] > value && map[i][j + 1] > value)
-                    lowest.Add(value);
-            }
-        }
-        else
-        {
-            if (j == 0)
-            {
-                if (map[i - 1][j] > value && map[i+1][j] > value && map[i][j + 1] > value)
-                    lowest.Add(value);
-            }
-            else if (j == map[i].Length - 1)
-            {
-                if (map[i - 1][j] > value && map[i + 1][j] > value && map[i][j - 1] > value)
-                    lowest.Add(value);
-            }
-            else
-            {
-                if (map[i - 1][j] > value && map[i+1][j] > value && map[i][j - 1] > value && map[i][j + 1] > value)
-                    lowest.Add(value);
-            }
-        }
+    if (neighbours.All(x => x.Value > point.Value))
+    {
+        lowest.Add(point.Value);
     }
 }
 
-int result = lowest.Sum(x => 1 + x);
-Console.WriteLine(result);
+int sum = map.Where(t => t.GetNeighbours().Select(x => map.FirstOrDefault(m => m.X == x.X && m.Y == x.Y)?.Value).All(x => t.Value < x.Value)).Sum(x => 1 + x.Value);
+
+Console.WriteLine(sum);
+
+
+class Point
+{
+    private int _width;
+    private int _height;
+
+    public Point(int x, int y, int value, int width, int height)
+    {
+        _width = width;
+        _height = height;
+
+        X = x;
+        Y = y;
+        Value = value;
+    }
+
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Value { get; set; }
+
+    public List<(int X, int Y)> GetNeighbours()
+    {
+        List<(int X, int Y)> neighbours = new List<(int X, int Y)> { (X-1, Y), (X+1, Y), (X, Y-1), (X, Y+1) };
+        return neighbours.Where(t => t.X >= 0 && t.X < _width && t.Y >= 0 && t.Y < _height).ToList();
+    }
+}
