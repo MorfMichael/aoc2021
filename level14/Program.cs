878a46bcc998b5b15ebe3abc23663ba5cdef31e9
Michael Morf <michael.morf@kcc.at>
2021-12-27 12:10:18 +0100

    day 14 part 2 win

--- level14/Program.cs
+++ level14/Program.cs
@@ -2,19 +2,38 @@
 string[] lines = File.ReadAllLines("input.txt");
 
 string template = lines.First();
+Dictionary<char, ulong> result = template.GroupBy(t => t).ToDictionary(t => t.Key, t => (ulong)t.Count());
 
-var rules = lines.Skip(2).ToDictionary(t => t[0..2], t => new string(new[] { t[0], t[6], t[1] }));
+var pairs = Enumerable.Range(0, template.Length - 1).Select(t => new string(template.Skip(t).Take(2).ToArray())).GroupBy(t => t).ToDictionary(x => x.Key, x => (ulong)x.Count());
+var rules = lines.Skip(2).ToDictionary(t => t[0..2], t => new List<string> { new string(new[] { t[0], t[6] }), new string(new[] { t[6], t[1] }) });
 
-List<string> result = new List<string>();
 
-for (int step = 0; step < 10; step++)
+for (int step = 0; step < 40; step++)
 {
-    var pairs = Enumerable.Range(0, template.Length - 1).Select(t => new string(template.Skip(t).Take(2).ToArray())).ToList();
-    template = template[0] + string.Join(string.Empty, pairs.Select(t => rules[t][1..]));
-    result.Add(template);
+    Dictionary<string, ulong> new_pairs = new Dictionary<string, ulong>();
+
+    foreach (var pair in pairs)
+    {
+        var rule = rules[pair.Key];
+
+        if (result.ContainsKey(rule[0][1]))
+            result[rule[0][1]]+= pair.Value;
+        else
+            result[rule[0][1]] = pair.Value;
+
+        if (new_pairs.ContainsKey(rule[0]))
+            new_pairs[rule[0]] += pair.Value;
+        else
+            new_pairs[rule[0]] = pair.Value;
+
+        if (new_pairs.ContainsKey(rule[1]))
+            new_pairs[rule[1]] += pair.Value;
+        else
+            new_pairs[rule[1]] = pair.Value;
+    }
+
+    pairs = new_pairs;
 }
 
-int max = result.Max(t => t.GroupBy(x => x).Select(x => x.Count()).Max());
-int min = result.Max(t => t.GroupBy(x => x).Select(x => x.Count()).Min());
-
+ulong min = result.Min(x => x.Value), max = result.Max(x => x.Value);
 Console.WriteLine($"{max} - {min} = {max - min}");
