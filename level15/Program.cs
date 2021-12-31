957031517eec15f112f2f4bfa736ca552ed50e5a
Michael Morf <michael.morf@kcc.at>
2021-12-31 13:11:02 +0100

    level15 tryout failed

--- level15/Program.cs
+++ level15/Program.cs
@@ -1,42 +1,56 @@
 ﻿string[] lines = File.ReadAllLines("example.txt");
 
-int[][] map = lines.Select(t => t.Select(x => int.Parse(x.ToString())).ToArray()).ToArray();
+Stack<Point> path = new Stack<Point>();
+List<Point> visited = new List<Point>();
 
+List<Point> map = lines.SelectMany((t,y) => t.Select((d,x) => new Point(x,y,int.Parse(d.ToString())))).ToList();
+PrintMap();
 
 int count = 0;
-int x = 0, y = 0;
-int width = map[0].Length, height = map.Length;
+Point start = map.First();
+Point bla = map.Skip(11).First();
+Point end = map.Last();
+List<Point> watched = new List<Point>();
 
-while (x < map[0].Length - 1 || y < map.Length - 1)
+int c = start.Count(map, end, watched);
+
+Point current = map.Last();
+
+Console.WriteLine(count);
+
+void PrintMap()
 {
-    Console.WriteLine(map[y][x]);
-    int right = Sum(x + 1, y);
-    int down = Sum(x, y + 1);
-
-    if (right <= down)
-    {
-        Console.WriteLine("down");
-        x++;
-    }
-    else
-    {
-        Console.WriteLine("right");
-        y++;
-    }
-
-    count += map[y][x];
+    Console.WriteLine(string.Join(Environment.NewLine, map.GroupBy(t => t.Y).Select(row => string.Join(string.Empty, row.Select(t => t.Value)))));
 }
 
-int Sum(int x, int y)
+class Point
 {
-    int count = map[y][x];
+    public Point(int x, int y, int value)
+    {
+        X = x;
+        Y = y;
+        Value = value;
+    }
 
-    if (x < map[0].Length - 1)
-        count += map[y][x + 1];
-    if (y < map.Length - 1)
-        count += map[y + 1][x];
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Value { get; set; }
 
-    return count;
+    public int Distance(Point point) => Math.Abs(point.X - X) + Math.Abs(point.Y - Y);
+
+    public int Count(List<Point> map, Point point, List<Point> watched)
+    {
+        watched.Add(this);
+
+        if (point == this)
+            return 1;
+
+        var ncount = map.Where(t => !watched.Contains(t) && IsNeighbour(t)).Select(t => t.Count(map, point, watched)).OrderBy(t => t).First();
+
+        return 1 + ncount;
+    }
+
+    public override string ToString() => $"{X},{Y},{Value}";
+
+    public bool IsNeighbour(Point point) => (point.X == X && (point.Y == Y-1 || point.Y == Y+1)) || (point.Y == Y && (point.X == X - 1 || point.X == X + 1));
 }
-
-Console.WriteLine($"{x}, {y}, {count}");
