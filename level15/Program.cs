8a929e35f2797578d27dc62f589f02cbd9141af8
Michael Morf <michael.morf@kcc.at>
2022-01-02 14:44:17 +0100

    level15 part 1 win

--- level15/Program.cs
+++ level15/Program.cs
@@ -1,42 +1,78 @@
-﻿string[] lines = File.ReadAllLines("example.txt");
+﻿//string[] lines = File.ReadAllLines("example.txt");
+string[] lines = File.ReadAllLines("input.txt");
 
-int[][] map = lines.Select(t => t.Select(x => int.Parse(x.ToString())).ToArray()).ToArray();
+List<Point> map = lines.SelectMany((t, y) => t.Select((d, x) => new Point(x, y, int.Parse(d.ToString())))).ToList();
 
+Point current = map.First();
+Point end = map.Last();
 
-int count = 0;
-int x = 0, y = 0;
-int width = map[0].Length, height = map.Length;
+List<Point> watched = new List<Point>();
+List<(Point Point, int Cost)> check = new List<(Point, int)> { (current, 0) };
 
-while (x < map[0].Length - 1 || y < map.Length - 1)
+(Point Point, int Cost) entry = check.First();
+
+while (check.Any())
 {
-    Console.WriteLine(map[y][x]);
-    int right = Sum(x + 1, y);
-    int down = Sum(x, y + 1);
+    entry = check.OrderBy(t => t.Cost).First();
+    check.Remove(entry);
 
-    if (right <= down)
+    if (watched.Contains(entry.Point))
+        continue;
+
+    if (entry.Point == end)
     {
-        Console.WriteLine("down");
-        x++;
-    }
-    else
-    {
-        Console.WriteLine("right");
-        y++;
+        break;
     }
 
-    count += map[y][x];
+    watched.Add(entry.Point);
+
+    var neighbours = map.Where(x => entry.Point.IsNeighbour(x)).ToList();
+
+    foreach (var neighbour in neighbours)
+    {
+        check.Add((neighbour, entry.Cost + neighbour.Value));
+    }
 }
 
-int Sum(int x, int y)
+Console.WriteLine(entry.Point);
+Console.WriteLine(entry.Cost);
+
+void PrintMap(List<Point> map, List<Point> path)
 {
-    int count = map[y][x];
+    List<List<Point>> grouped = map.GroupBy(t => t.Y).Select(t => t.ToList()).ToList();
 
-    if (x < map[0].Length - 1)
-        count += map[y][x + 1];
-    if (y < map.Length - 1)
-        count += map[y + 1][x];
+    foreach (var row in grouped)
+    {
+        foreach (var point in row)
+        {
+            if (path.Contains(point))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
 
-    return count;
+            Console.Write(point.Value);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        Console.WriteLine();
+    }
 }
 
-Console.WriteLine($"{x}, {y}, {count}");
+class Point
+{
+    public Point(int x, int y, int value)
+    {
+        X = x;
+        Y = y;
+        Value = value;
+    }
+
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Value { get; set; }
+
+    public int Distance(Point point) => Math.Abs(point.Y - Y) + Math.Abs(point.X - X);
+
+    public bool IsNeighbour(Point point) => (point.X == X && (point.Y == Y - 1 || point.Y == Y + 1)) || (point.Y == Y && (point.X == X - 1 || point.X == X + 1));
+
+    public override string ToString() => $"{X};{Y};{Value}";
+}
