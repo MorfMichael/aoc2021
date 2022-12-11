458390e375728f1bb5907731c750a2b2db840b66
Ing. Morf Michael <michael.morf@gmx.at>
2022-12-11 14:48:46 +0100

    level21 part2

--- level21/Program.cs
+++ level21/Program.cs
@@ -1,48 +1,101 @@
-﻿int p1pos = 5;
-int p2pos = 6;
+﻿Dictionary<(int p1, int p2, int s1, int s2),(long sc1, long sc2)> states = new();
 
-int p1score = 0;
-int p2score = 0;
+var result = Roll(5,6,0,0);
 
-int cur = 1;
 
-bool player1 = true;
+Console.WriteLine(result.s1);
+Console.WriteLine(result.s2);
 
-while (p1score < 1000 && p2score < 1000)
+(long s1, long s2) Roll(int p1, int p2, int s1, int s2)
 {
-    int sum = cur * 3 + 3;
-    Console.WriteLine(sum);
-    if (player1)
+    if (s1 >= 21)
+        return (1, 0);
+
+    if (s2 >= 21)
+        return (0, 1);
+
+    if (states.ContainsKey((p1, p2, s1, s2)))
+        return states[(p1, p2, s1, s2)];
+
+    (long s1, long s2) ans = (0, 0);
+
+    for (int i = 1; i <= 3; i++)
     {
-        Console.WriteLine(p1pos);
-        int pos = p1pos + sum;
-        p1pos = pos%10 == 0 ? 10 : pos%10;
-        p1score += p1pos;
-        Console.WriteLine(p1pos);
+        for (int j = 1; j <= 3; j++)
+        {
+            for (int k = 1; k <= 3; k++)
+            {
+                int sum = i + j + k;
+
+                int pos = p1 + sum;
+                int newp1 = pos % 10 == 0 ? 10 : pos % 10;
+                int news1 = s1 + newp1;
+
+                (long sc1, long sc2) = Roll(p2, newp1, s2, news1);
+                ans = (ans.s1 + sc2, ans.s2 + sc1);
+            }
+        }
     }
-    else
-    {
-        Console.WriteLine(p2pos);
-        int pos = p2pos + sum;
-        p2pos = pos%10 == 0 ? 10 : pos%10;
-        p2score += p2pos;
-        Console.WriteLine(p2pos);
-    }
-    cur += 3;
-    player1 = !player1;
+    states.Add((p1, p2, s1, s2), ans);
+    return ans;
 }
 
-if (p1score >= 1000)
-{
-    Console.WriteLine("Player 2 lost");
-    Console.WriteLine(p2score);
-    Console.WriteLine(cur-1);
-    Console.WriteLine(p2score * (cur-1));
-}
-else
-{
-    Console.WriteLine("Player 1 lost");
-    Console.WriteLine(p1score);
-    Console.WriteLine(cur-1);
-    Console.WriteLine(p1score * (cur-1));
-}
+//class Quantum
+//{
+//    public int P1Score { get; set; }
+//    public int P2Score { get; set; }
+
+//    public int P1Pos { get; set; }
+//    public int P2Pos { get; set; }
+
+//    public bool Player1 { get; set; }
+
+//    public (int p1, int p2) Roll()
+//    {
+//        if (P1Score >= 21)
+//            return (1, 0);
+
+//        if (P2Score >= 21)
+//            return (0, 1);
+
+//        (int s1, int s2) ans = (0, 0);
+
+//        for (int i = 1; i <= 3; i++)
+//        {
+//            for (int j = 1; j <= 3; j++)
+//            {
+//                for (int k = 1; k <= 3; k++)
+//                {
+//                    int sum = i + j + k;
+
+//                    if (Player1)
+//                    {
+//                        int pos = P1Pos + sum;
+//                        P1Pos = pos % 10 == 0 ? 10 : pos % 10;
+//                        P1Score += P1Pos;
+//                    }
+//                    else
+//                    {
+//                        int pos = P2Pos + sum;
+//                        P2Pos = pos % 10 == 0 ? 10 : pos % 10;
+//                        P2Score += P2Pos;
+//                    }
+
+//                    (int s1, int s2) = this.Copy().Roll();
+//                    ans = (ans.s1 + s1, ans.s2 + s2);
+//                }
+//            }
+//        }
+
+//        return ans;
+//    }
+
+//    public Quantum Copy() => new Quantum
+//    {
+//        P1Score = P1Score,
+//        P2Score = P2Score,
+//        P1Pos = P1Pos,
+//        P2Pos = P2Pos,
+//        Player1 = !Player1
+//    };
+//}
