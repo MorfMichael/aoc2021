34167c1f1be639ea6f26d5dbe1b199b4b9264714
Michael Morf <michael.morf@kcc.at>
2022-12-14 16:27:52 +0100

    level22 other samples

--- level22/Program.cs
+++ level22/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics;
 
-string[] lines = File.ReadAllLines("level22.in");
+string[] lines = File.ReadAllLines("sample2.in");
 
 HashSet<(int x, int y, int z)> cubes = new();
 
@@ -14,21 +14,29 @@ foreach (var line in lines)
     int[] ys = coords[1][2..].Split("..").Select(int.Parse).ToArray();
     int[] zs = coords[2][2..].Split("..").Select(int.Parse).ToArray();
 
+    long xr = xs[1] - xs[0];
+    long yr = ys[1] - ys[0];
+    long zr = zs[1] - zs[0];
+
+    Console.WriteLine($"x: {xr} * y: {yr} * z: {zr} = {xr*yr*zr}");
+
+    HashSet<(int x, int y, int z)> check = new();
     for (int x = xs[0]; x <= xs[1]; x++)
     {
-        if (x < -50 || x > 50) continue;
+        //if (x < -50 || x > 50) continue;
         for (int y = ys[0]; y <= ys[1]; y++)
         {
-        if (y < -50 || y > 50) continue;
+            //if (y < -50 || y > 50) continue;
             for (int z = zs[0]; z <= zs[1]; z++)
             {
-            if (z < -50 || z > 50) continue;
-                var value = (x, y, z);
-                if (state == "on" && !cubes.Contains(value)) cubes.Add(value);
-                if (state == "off" && cubes.Contains(value)) cubes.Remove(value);
+                //if (z < -50 || z > 50) continue;
+                check.Add((x, y, z));
             }
         }
     }
+
+    if (state == "on") foreach (var c in check) cubes.Add(c);
+    if (state == "off") foreach (var c in check) cubes.Remove(c);
 }
 
 Console.WriteLine(cubes.Count);
